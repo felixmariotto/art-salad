c3df58975b52a421bc9d11836a65a35dd628f0f9
Felix Mariotto <felixmariotto@gmail.com>
2022-08-29 23:25:15 +0200

    add flat github button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import stage from './misc/stage.js';
 import UI from './UI.js';
 import gameManager from './gameManager.js';
 
+import ghIconURL from '../assets/UI-images/github-icon.png';
+
 //
 
 scene.add( stage, UI.group, UI.loadingGroup );
@@ -22,3 +24,22 @@ setTimeout( ()=> {
 	// gameManager.startPuzzle( 'pentecostRederos' )
 }, 500 );
 */
+
+const ghLink = document.createElement('A');
+ghLink.href = 'https://github.com/felixmariotto/art-salad';
+ghLink.target = '_blank';
+
+ghLink.style.position = 'absolute';
+ghLink.style.right = '15px';
+ghLink.style.bottom = '15px';
+ghLink.style.width = '44px';
+ghLink.style.height = '44px';
+ghLink.style.backgroundColor = 'white';
+ghLink.style.backgroundImage = `url(${ ghIconURL })`;
+ghLink.style.backgroundSize = 'contain';
+ghLink.style.borderRadius = '26px';
+ghLink.style.borderWidth = '4px';
+ghLink.style.borderStyle = 'solid';
+ghLink.style.borderColor = 'white';
+
+document.body.appendChild( ghLink );
